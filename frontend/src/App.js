082b6c58aa207b2a755049995d6fc2084134c242
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2025-08-05 22:41:32 +0800

    Added mantine UI library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import "./styles/App.css";
+import "@mantine/core/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
 import BuyerDashboard from "./pages/BuyerDashboard.js";
+
+const theme = createTheme({
+  // Customize your theme here
+});
 
 function App() {
   return (
-    <div className="App">
-      <BuyerDashboard />
-    </div>
+    <MantineProvider theme={theme}>
+      <div className="App">
+        <BuyerDashboard />
+      </div>
+    </MantineProvider>
   );
 }
 
